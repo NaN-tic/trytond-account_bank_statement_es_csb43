25755555a97d08c5b6d0cad04441f41573cfd0ad
resteve <resteve@zikzakmedia.com>
2015-01-21 13:08:28 +0100

    pyflakes

--- statement.py
+++ statement.py
@@ -1,7 +1,7 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
 from trytond.pool import Pool
-from trytond.model import ModelSQL, ModelView, fields
+from trytond.model import ModelView, fields
 from trytond.transaction import Transaction
 from trytond.wizard import Wizard, StateView, StateTransition, Button
 from retrofix import c43
