e9165d9c0234daeff3cf138b6b286dd33e1a4714
Raimon Esteve <raimon@nan-tic.com>
2016-12-12 10:43:04 +0100

    Add scenario bank statement CSB43 test

--- tests/test_account_bank_statement_es_csb43.py
+++ tests/test_account_bank_statement_es_csb43.py
@@ -2,17 +2,24 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import (doctest_setup, doctest_teardown,
+    doctest_checker)
 
 
-class AccountBankStatementEsCsb43TestCase(ModuleTestCase):
-    'Test Account Bank Statement Es Csb43 module'
+class AccountBankStatementESCSB43TestCase(ModuleTestCase):
+    'Test Account Bank Statement ES CSB43 module'
     module = 'account_bank_statement_es_csb43'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AccountBankStatementEsCsb43TestCase))
+        AccountBankStatementESCSB43TestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_bank_statement_es_csb43.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+            checker=doctest_checker))
     return suite
