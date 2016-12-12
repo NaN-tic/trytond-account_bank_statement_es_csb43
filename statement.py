2a10f2fd4afdcd9b882d151b0d80f1ecb176f937
Raimon Esteve <raimon@nan-tic.com>
2016-12-12 10:40:53 +0100

    Call search_reconcile to Bank Statement class

--- statement.py
+++ statement.py
@@ -125,7 +125,7 @@ class ImportCSB43(Wizard):
 
         if has_confirm:
             BankStatement.confirm([statement])
-            statement.search_reconcile()
+            BankStatement.search_reconcile([statement])
 
         if has_attachment:
             attach = Attachment(
